f6000abdea804485ba50130e0f525a0d77ebbf4a
Hamza Khan <112khanhamza@gmail.com>
2024-05-26 18:52:29 +0530

    Updated as gradle project

---
+++ src/main/scala/com/hamza/polymorphism/FruitExample.scala
@@ -0,0 +1,27 @@
+package com.hamza.polymorphism
+
+class Skin {
+
+}
+
+class Fruit {
+    def canEatSkin: Boolean = true
+    def fractionalLiquidContent: Double = 0.5
+    def peel(): Skin = ???
+    def slice(): Unit = ???
+}
+
+class FruitExample {
+
+//    makeBreakfastShake(new Banana())
+//    makeBreakfastShake(new Apple())
+//
+//    def makeBreakfastShake(fruit: Fruit): Unit = {
+//        if (!fruit.canEatSkin) fruit.peel()
+//        blender += fruit
+//        blender += juice
+//        if (fruit.fractionalLiquidContent < 0.3) blender += juice
+//        blender += ice
+//        blender.blend
+//    }
+}
